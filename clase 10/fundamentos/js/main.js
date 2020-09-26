a82b76a8680ce5fb34548328beb8d7d1b0306fbf
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-09-25 18:22:32 -0600

    Variables y Cadenas

---
+++ clase 10/fundamentos/js/main.js
@@ -0,0 +1,23 @@
+//alert('Hola Mundo!!!');
+//document.getElementById('encabezado').innerHTML = "Bienvenido a este curso";
+
+//variables
+var miVariable = 10;
+let miOtraVariable = "Hola";
+const pi = 3.1415;
+
+//reasignacion
+miVariable = 20;
+miOtraVariable = "Bienvenido";
+
+let mi_arreglo = [1,2,"Yesser",true];
+console.log(mi_arreglo[2]);
+
+//operadores de asignacion
+let suma = 5+2; //7
+suma += 2; //suma = suma + 2 = 9
+console.log("Suma : " + suma);
+suma -= 2; // 7
+console.log(`Resta: ${suma}`);
+suma *= 3; // 21
+console.log('Multiplicacion: ', suma);
